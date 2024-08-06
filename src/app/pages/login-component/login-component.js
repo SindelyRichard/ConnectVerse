0afe3addde969bc9140f7dbf5f75688d1c17cfb4
SindelyRichard <rsindely@gmail.com>
2024-08-06 18:28:03 +0200

    Login component design finished

--- src/app/pages/login-component/login-component.js
+++ src/app/pages/login-component/login-component.js
@@ -5,22 +5,22 @@ export default function Login(){
     return(
         <div className={styles.login_container}>
             <form>
-                <textarea 
+                <textarea className={styles.login_form}
                 maxLength={20}
                 placeholder='Username'
                 
                 />
             </form>
             <form>
-                <textarea 
+                <textarea className={styles.login_form}
                 maxLength={20}
                 placeholder='Password'
                 
                 />
             </form>
             <div>
-            <button type="submit">Login</button>
-            <button type="submit">Register</button>
+            <button className={styles.login_button} type="submit">Login</button>
+            <button className={styles.register_button} type="submit">Register</button>
             </div>
         </div>
     );
